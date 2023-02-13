88ad0ff79e80c4860f4712433ef236db5a832100
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-13 15:06:53 +0100

    add home page

--- app.js
+++ app.js
@@ -21,8 +21,16 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
 // setting the spotify-api goes here:
 
 // Our routes go here:
+app.get("/", (req, res, next) => {
+    try {
+        res.render("home");
+    } catch(error) {
+        next(error);
+    }
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
